31b161fa508d5c78d0cc344384e7a3b845b1bf97
mimi_cho <hold1593@naver.com>
2021-06-11 15:50:45 +0900

    :wrench: conflict resolve

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,15 +2,35 @@ import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
 import Nav from '../components/Nav';
 import logo from '../img/simsimae_logo.png';
+import qr from '../img/simsimae_qr.png';
+import $ from "jquery";
 
 class Main extends Component {
   constructor(props){
     super(props);
+    this.revealQr = this.revealQr.bind(this);
+    this.qrRequestHandler = this.qrRequestHandler.bind(this);
     this.state = {
       email: '',
       password: '',
+      click: false
     }
+
   }
+
+  qrRequestHandler() {
+    
+  };
+
+  revealQr(){
+    $(".body").css('display', 'none');
+    $(".center-qr").css('display', 'block');
+    $(".button").css('display', 'block');
+    
+  }
+
+    
+ 
 
   render() {
     return (
@@ -28,7 +48,11 @@ class Main extends Component {
                   </label>
               </div> */}
               <div><img className='center-logo' src={logo} alt=""></img></div>
-              <div><button onClick={this.qrRequestHandler} className="qrBtn">Click!</button></div>
+              <div><button onClick={this.revealQr} className = "qrBtn">Click!</button></div>
+          </div>
+          <div className="reveal">
+              <div><img className='center-qr' src={qr} alt=""></img></div>
+              <div><button onClick={this.qrRequestHandler} className="button">🔄</button></div>
           </div>
 
           <div className="footer">
