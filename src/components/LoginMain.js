ff46cfbc88a4828b9bfa70f6c0b71876206955ef
Song WonJoon <wonjoon0730@gmail.com>
2021-06-15 20:07:00 +0900

    qrRender changed

--- src/components/LoginMain.js
+++ src/components/LoginMain.js
@@ -1,15 +1,57 @@
 import React, { useState } from "react";
 import { withRouter, useHistory } from "react-router-dom";
 
-const LoginMain = ({closeModal}) => {
+const LoginMain = ({closeModal, qrRequestHandler, qrImg, userQrRequestHandler, userQrImg}) => {
+  const [toggleOn, setToggleOn ] = useState(true);
+  const toggleStatus = () => {
+    if(toggleOn === true) {
+      setToggleOn(false);
+    }else if(toggleOn === false) {
+      setToggleOn(true);
+    }
+  }
+
   
+
   closeModal()
   return(
-    <>
+    
       <div className="body">
-        <h1>로그인했땅!</h1>
-      </div>
-    </>
+          <div className='toggle-div'>
+              <input className='toggle-input' type="checkbox" id="switch"/>
+              <label className='toggle-label'htmlFor="switch" onClick={() => {toggleStatus()}}></label>
+          </div>
+            {toggleOn ? 
+            //토글 켜진 QR
+              <div className="qrRender">
+                <div className='center-qr'>
+                  <img src={userQrImg} alt=''/>
+                  <a href='http://www.naver.com' target='_blank'>
+                  </a>
+                </div>
+                <div className='reBtnDiv'>
+                  <button onClick={() => userQrRequestHandler()} className="refreshBtn">
+                  <i className="fas fa-sync-alt"></i>
+                  </button>
+                </div>
+              </div>: 
+          //토글 꺼진 QR
+              <div className="qrRender">
+              <div className='center-qr'>
+                <img src={qrImg} alt=''/>
+                <a href='http://www.naver.com' target='_blank'>
+                </a>
+              </div>
+              <div className='reBtnDiv'>
+                <button onClick={() => qrRequestHandler()} className="refreshBtn">
+                <i className="fas fa-sync-alt"></i>
+                </button>
+              </div>
+            </div>
+            }
+            
+        </div>
+    
   )
 }
 
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -15,13 +15,12 @@ const Main = () => {
   const [password , setPassword ] = useState('');
   const [token , setToken ] = useState('Bearer ');
   const [errorMessage , setErrorMessage ] = useState('');
-  const [toggleOn, setToggleOn ] = useState();
   const [isLogin , setIsLogin ] = useState(false);
   const [isGoogleLogin, setIsGoogleLogin ] = useState(false);
   const [userinfo , setUserinfo] = useState(null);
   const [isModalOpen , setisModalOpen] = useState(false);
   const [qrImg , setQrImg ] = useState(null);
-  
+  const [userQrImg, setUserQrImg ] = useState(null);
   // 이메일 상태값 변경
   function emailHandler(e) {
     setEmail(e.target.value);
@@ -65,17 +64,14 @@ const Main = () => {
       setQrImg(res.data)
     })
   }; 
-  // 토글 스테이트 핸들러
-  const toggleStatus = () => {
-    if(isLogin || isGoogleLogin){
-      $(".toggle-div").css('display', 'block');
-      if(toggleOn === false) {
-        //비회원로그인 qr
-      }else if(toggleOn === true) {
-        //회원로그인 qr
-      }
-    }
+  const userQrRequestHander = () => {
+    return axios.get('http://13.209.10.136/url/userurl')
+    .then((res) => {
+      setUserQrImg(res.data)
+    })
   }
+  // 토글 스테이트 핸들러
+  
   // qr다시 받기 핸들러
   const revealQr = () => {
     $(".logoRender").css('display', 'none');
@@ -122,14 +118,17 @@ const Main = () => {
 
       { isLogin ? 
       <Link to='/'>
-        <LoginMain closeModal={closeModal} handleResponseSuccess={handleResponseSuccess}/> 
+        <LoginMain 
+          closeModal={closeModal} 
+          handleResponseSuccess={handleResponseSuccess}
+          qrImg={qrImg} 
+          qrRequestHandler={qrRequestHandler}
+          userQrRequestHander={userQrRequestHander}
+          userQrImg={userQrImg}
+        /> 
       </Link>
         :
         <div className="body">
-          <div className='toggle-div'>
-              <input className='toggle-input' type="checkbox" id="switch" />
-              <label className='toggle-label'htmlFor="switch"></label>
-            </div>
             <div className='logoRender'>
               <div><img className='center-logo' src={logo} alt=""></img></div>
               <div><button onClick={() => revealQr()} className = "qrBtn">Click!</button></div>
