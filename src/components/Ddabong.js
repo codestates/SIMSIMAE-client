e07a64455dca86c03b65161f3e2c9285f6145c93
Song WonJoon <wonjoon0730@gmail.com>
2021-06-17 02:19:05 +0900

    temp

--- src/components/Ddabong.js
+++ src/components/Ddabong.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { withRouter } from "react-router-dom";
 import FavoriteCheck from "./FavoriteCheck";
 import '../css/Like.css';
+
 import like_btn from '../img/like_btn.png';
 import like_btn_filled from '../img/like_btn_filled.png';
 import dislike_btn from '../img/dislike_btn.png';
@@ -9,9 +10,10 @@ import dislike_btn_filled from '../img/dislike_btn_filled.png';
 import axios from 'axios';
 import { get } from "jquery";
 
-const Ddabong = ({userinfo, setUserinfo, accessToken}) => {
+const Ddabong = ({accessToken}) => {
     const [isUserLike, setIsUserLike ] = useState(false);
     const [isUserDislike, setIsUserDislike ] = useState(false);
+    const [userinfo, setUserinfo] = useState(null);
     
     
     const userClickLikeBtn = () => {
@@ -23,8 +25,8 @@ const Ddabong = ({userinfo, setUserinfo, accessToken}) => {
 
             }
             setIsUserLike(true);
-            // getUserInfo();
-            // sendToLikeQr();
+            getUserInfo();
+            sendToLikeQr();
             
         //이미 like 버튼이 눌려있으면
         }else if(isUserLike === true) {
@@ -48,30 +50,39 @@ const Ddabong = ({userinfo, setUserinfo, accessToken}) => {
             setIsUserDislike(false);
         }
     }
-    const getUserInfo = () => {
-      axios.get('http://13.209.10.136/user/info',
-        { headers : {authorization: accessToken , withCredentials: true}
-        })
-        .then(res => {
-            console.log('user 정보 받아오기 성공!!!', res)
-            setUserinfo(res.data.userInfo);
-        }).catch(err => console.log(err));
-    }
     
+    const getUserInfo = () => {
+        axios.get('http://13.209.10.136/user/info',
+            { headers : {authorization: accessToken , withCredentials: true}
+            })
+            .then(res => {
+                console.log('user 정보 받아오기 성공!!!', res)
+                const data = res.data;
+                setUserinfo(data)
+                console.log(userinfo)
+                
+                
+        })
+    }
     
     const sendToLikeQr = () => {
-        console.log(userinfo);
-        axios.post('http://13.209.10.136/url/like', 
+            const url = "http://naver.com";
+            const userId = userinfo.userInfo.id;
+            axios.post('http://13.209.10.136/url/like',
+                        {userId:{userId}, url:{url}},
+                        { 'Content-Type':'application/json', withCredentials: true }
+                    ).then((res) => {
+                        console.log('관심QR에 저장했습니다!')
+                    }).catch((err) => console.log(err))
             
-            { 'Content-Type':'application/json', withCredentials: true }
-        ).then((res) => {
-            console.log('관심QR에 저장했습니다!')
-        }).catch((err) => console.log(err))
     }
+        
+            
+    
             
     return(
         <>
-            {isUserLike ? <img className='likeBtn' onClick={() => getUserInfo()} src={like_btn_filled}/> :
+            {isUserLike ? <img className='likeBtn' onClick={() => userClickLikeBtn()} src={like_btn_filled}/> :
             <img className='likeBtn' onClick={() => userClickLikeBtn()} src={like_btn}/>}
             {isUserDislike ? <img className='dislikeBtn'onClick={() => userClickDislikeBtn()} src={dislike_btn_filled}/> :
             <img className='dislikeBtn'onClick={() => userClickDislikeBtn()} src={dislike_btn}/>}
