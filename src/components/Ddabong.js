496c85aa9e3f600b57a137922b476a6ea6758bed
Song WonJoon <wonjoon0730@gmail.com>
2021-06-17 11:12:52 +0900

    upstream merge

--- src/components/Ddabong.js
+++ src/components/Ddabong.js
@@ -1,92 +1,27 @@
-import React, { useState } from "react";
+import React from "react";
 import { withRouter } from "react-router-dom";
-import FavoriteCheck from "./FavoriteCheck";
 import '../css/Like.css';
-
 import like_btn from '../img/like_btn.png';
 import like_btn_filled from '../img/like_btn_filled.png';
 import dislike_btn from '../img/dislike_btn.png';
 import dislike_btn_filled from '../img/dislike_btn_filled.png';
-import axios from 'axios';
-import { get } from "jquery";
 
-const Ddabong = ({userinfo, setUserinfo, accessToken}) => {
-    const [isUserLike, setIsUserLike ] = useState(false);
-    const [isUserDislike, setIsUserDislike ] = useState(false);
-    let userId;
-    let url;
-    
-    
-    const userClickLikeBtn = () => {
-        //like 버튼이 눌려있지 않을 때
-        if(isUserLike === false) {
-            //만약 Dislike 버튼이 눌려있으면 취소되고
-            if(isUserDislike === true) {
-                setIsUserDislike(false);
+const Ddabong = ({isUserLike, isUserDislike, dislikeSelect, likeSelect}) => {
 
-            }
-            setIsUserLike(true);
-            sendToLikeQr();
-            
-        //이미 like 버튼이 눌려있으면
-        }else if(isUserLike === true) {
-            //like 버튼을 취소한다
-            setIsUserLike(false);
-        }
+  return(
+    <>
+    { !isUserLike ? 
+    <i className="far fa-thumbs-up likeBtn" onClick={() => likeSelect()}/> 
+    :
+    <i className="far fa-thumbs-up clickLikeBtn" onClick={() => likeSelect()}/>
     }
-
-    const userClickDislikeBtn = () => {
-        //dislike 버튼이 눌려있지 않을 때
-        if(isUserDislike === false) {
-            //만약 like 버튼이 눌려있으면 취소되고
-            if(isUserLike === true) {
-                setIsUserLike(false);
-            }
-            // dislike 버튼이 눌린다
-            setIsUserDislike(true);
-        //이미 dislike 버튼이 눌려있으면
-        }else if(isUserDislike === true) {
-            //dislike 버튼을 취소한다
-            setIsUserDislike(false);
-        }
+    {
+    !isUserDislike ?
+    <i class="far fa-thumbs-down dislikeBtn" onClick={() => dislikeSelect()}/>
+    :
+    <i class="far fa-thumbs-down clickDislikeBtn" onClick={() => dislikeSelect()}/>
     }
-    
-    const getUserInfo = () => {
-        axios.get('http://13.209.10.136/user/info',
-            { headers : {authorization: accessToken , withCredentials: true}
-            })
-            .then(res => {
-                console.log('user 정보 받아오기 성공!!!', res)
-                const data = res.data;
-                userId = data.userInfo.id;
-                url = data.likeUrl;
-                console.log
-                
-        })
-    }
-    
-    const sendToLikeQr = () => {
-        
-        
-        getUserInfo();
-        return axios.post('http://13.209.10.136/url/like', 
-                        {userId, url},
-                        { 'Content-Type':'application/json', withCredentials: true }
-                    ).then((res) => {
-                        console.log('관심QR에 저장했습니다!')
-                    }).catch((err) => console.log(err))
-        }
-            
-    
-            
-    return(
-        <>
-            {isUserLike ? <img className='likeBtn' onClick={() => userClickLikeBtn()} src={like_btn_filled}/> :
-            <img className='likeBtn' onClick={() => userClickLikeBtn()} src={like_btn}/>}
-            {isUserDislike ? <img className='dislikeBtn'onClick={() => userClickDislikeBtn()} src={dislike_btn_filled}/> :
-            <img className='dislikeBtn'onClick={() => userClickDislikeBtn()} src={dislike_btn}/>}
-            
-        </>
-    )
+    </>
+  )
 }
 export default withRouter(Ddabong); 
